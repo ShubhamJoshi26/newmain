d14594db188d61870adcfa14e78acf28a4ce1e1c
ShubhamJoshi26 <101936053+ShubhamJoshi26@users.noreply.github.com>
2022-10-04 16:57:15 +0530

    new changes

---
+++ templates/user.js
@@ -0,0 +1,6 @@
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+var jQueryScript1 = document.createElement('script');  
+jQueryScript1.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js');
+document.head.appendChild(jQueryScript1);
